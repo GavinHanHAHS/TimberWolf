7d306a02a85449b8f43fe4e8179651332c17a791
GavinHanHAHS <55220804+GavinHanHAHS@users.noreply.github.com>
2020-10-16 15:09:43 -0600

    better logs, branches, character and start of gameover

--- main.js
+++ main.js
@@ -19,48 +19,57 @@ let keydown = false;
 
 let playerSide = -1;
 
-logs.push({ // first log always empty
-  x: (cnv.width / 2) - 75,
-  y: 0,
-  direction: 0,
-  position: 0,
-  angle: 0,
-  angleChange: Math.random() * 10,
-  branch: 0
-})
 
-for(let i = 1; i < 8; i++) { // add 7 more logs to complete tree
-  let branch = Math.round(Math.random() * 3 - 1.5);
-  if(branch == -0) {branch = 0};
-
-  logs.push({
-    x: (cnv.width / 2) - 75,
-    y: 0,
-    direction: 0,
-    position: i,
-    angle: 0,
-    angleChange: Math.random() * 10 - 5,
-    branch: branch
-  })
-}
-
-
-// things to do:
-// add player character
-// branches array and drawing
-// ~> seperate array for branches, with values -1, 0 or 1
-// additional images for log + branch for when they fly
+// things to do?:
 // day/night cycle
 
 
 
 function start() { // waits for body to load, then loads
   images = {       // images and starts game
-    log: document.getElementById("log"),
     stump: document.getElementById("stump"),
     background: document.getElementById("background"),
-    branch: document.getElementById("branch"),
-    werewolf: document.getElementById("werewolf")
+    branchR: document.getElementById("branchR"),
+    werewolf: document.getElementById("werewolf"),
+    branchL: document.getElementById("branchL"),
+    tombstone: document.getElementById("tombstone"),
+    gameover: document.getElementById("gameover")
+  }
+
+  logs.push({ // first log always empty
+    x: (cnv.width / 2) - 75,
+    y: 0,
+    direction: 0,
+    position: 0,
+    angle: 0,
+    angleChange: Math.random() * 10,
+    branch: 0,
+    img: document.getElementById("log")
+  })
+  
+  for(let i = 1; i < 8; i++) { // add 7 more logs to complete tree
+    let log = {
+      x: (cnv.width / 2) - 75,
+      y: 0,
+      direction: 0,
+      position: i,
+      angle: 0,
+      angleChange: Math.random() * 10 - 5,
+      branch: 0,
+      img: document.getElementById("log")
+    };
+
+    log.branch = Math.round(Math.random() * 3 - 1.5);
+    if(log.branch == -0) {log.branch = 0};
+
+    if(Math.random() >= 0.35) {
+      log.img = document.getElementById("log");
+    } else {
+      log.img = document.getElementById("log2");
+    }
+  
+  
+    logs.push(log);
   }
 
   requestAnimationFrame(main);
@@ -78,17 +87,12 @@ function main() {
       i--;
     } else { // otherwise draw it
       if(logs[i].position >= 0) {
-        ctx.drawImage(images.log, logs[i].x, 455 - (logs[i].position * 75) - logYModify);
-        if(logs[i].branch != 0) {
+        ctx.drawImage(logs[i].img, logs[i].x, 430 - (logs[i].position * 100) - logYModify);
+        if(logs[i].branch != 0) { // draw branches
           if(logs[i].branch == -1) {
-            ctx.drawImage(images.branch, logs[i].x + 150, 455 - (logs[i].position * 75) - logYModify);
+            ctx.drawImage(images.branchR, logs[i].x + 144, 430 - (logs[i].position * 100) - logYModify);
           } else if(logs[i].branch == 1) { //redundant
-            ctx.save();
-            ctx.translate(100, 492.5 - (logs[i].position * 75) - logYModify);
-            ctx.rotate(180 * Math.PI / 180);
-            ctx.translate(-(logs[i].x + 75), -(logs[i].y + 37.5));
-            ctx.drawImage(images.branch, logs[i].x, logs[i].y)
-            ctx.restore();
+            ctx.drawImage(images.branchL, logs[i].x - 95, 430 - (logs[i].position * 100) - logYModify);
           }
           
         }
@@ -97,17 +101,13 @@ function main() {
         ctx.translate(logs[i].x + 75, logs[i].y + 37.5);
         ctx.rotate(logs[i].angle * Math.PI / 180);
         ctx.translate(-(logs[i].x + 75), -(logs[i].y + 37.5));
-        ctx.drawImage(images.log, logs[i].x, logs[i].y)
+        ctx.drawImage(logs[i].img, logs[i].x, logs[i].y)
+        
         if(logs[i].branch != 0) {
           if(logs[i].branch == -1) {
-            ctx.drawImage(images.branch, logs[i].x + 150, logs[i].y);
+            ctx.drawImage(images.branchR, logs[i].x + 150, logs[i].y);
           } else if(logs[i].branch == 1) { //redundant
-            ctx.save();
-            ctx.translate(logs[i].x - 50, logs[i].y - 37.5);
-            ctx.rotate(180 * Math.PI / 180);
-            ctx.translate(-(logs[i].x - 50), -(logs[i].y - 37.5));
-            ctx.drawImage(images.branch, logs[i].x - 100, logs[i].y - 150);
-            ctx.restore();
+            ctx.drawImage(images.branchL, logs[i].x - 95, 430 - (logs[i].position * 100) - logYModify);
           }
         }
         ctx.restore();
@@ -129,8 +129,7 @@ function main() {
     ctx.fillStyle = "dimgray";
     ctx.fillRect(100, 150, 300, 200);
 
-    //ctx.fillStyle = "slateBlue";
-    //ctx.fillRect(cnv.width / 2 + (135 * playerSide), 475, 40, 40);
+    ctx.drawImage(images.werewolf, cnv.width / 2 + (225 * playerSide), 400, 200, 150);
 
     ctx.fillStyle = "white";
     ctx.font = "42px Arial Black"
@@ -141,26 +140,37 @@ function main() {
     ctx.fillText("to Start", 135, 330);
   } else if(gamestate == 1) {
     // game is playing
+    if(playerSide == -1) {
+      ctx.drawImage(images.werewolf, cnv.width / 2 + (225 * playerSide), 400, 200, 150);
+    } else {
+      ctx.save();
+      ctx.translate(cnv.width / 2 + (35 * playerSide), 400);
+      ctx.scale(-1, 1)
+      ctx.translate(-(cnv.width / 2 + (35 * playerSide)), -400);
+      ctx.drawImage(images.werewolf, cnv.width / 2 - (155 * playerSide), 400, 200, 150);
+      ctx.restore();
+    }
   } else if(gamestate == 2) { // technically redundant
     //game over
-    //ctx.fillStyle = "black";
-    //ctx.fillRect(50, 50, cnv.width / 2, cnv.height / 2);
+    if(playerSide == -1) {
+      ctx.drawImage(images.tombstone, cnv.width / 2 + (225 * playerSide), 400, 200, 150);
+    } else {
+      ctx.save();
+      ctx.translate(cnv.width / 2 + (35 * playerSide), 400);
+      ctx.scale(-1, 1)
+      ctx.translate(-(cnv.width / 2 + (35 * playerSide)), -400);
+      ctx.drawImage(images.tombstone, cnv.width / 2 - (155 * playerSide), 400, 200, 150);
+      ctx.restore();
+    }
+
+    ctx.fillStyle = "dimGray";
+    ctx.fillRect()
+    ctx.drawImage(images.gameover, 100, 100);
   }
   
   // draw player
   ctx.fillStyle = "slateBlue";
-  if(playerSide == -1) {
-    //ctx.fillRect(cnv.width / 2 + (135 * playerSide), 475, 40, 40);
-    ctx.drawImage(images.werewolf, cnv.width / 2 + (275 * playerSide), 275);
-  } else {
-    //ctx.fillRect(cnv.width / 2 + (100 * playerSide), 475, 40, 40);
-    ctx.save();
-    ctx.translate(cnv.width / 2 + (35 * playerSide), 400);
-    ctx.scale(-1, 1)
-    ctx.translate(-(cnv.width / 2 + (35 * playerSide)), -400);
-    ctx.drawImage(images.werewolf, cnv.width / 2 - (215 * playerSide), 275);
-    ctx.restore();
-  }
+
   
 
 
@@ -168,15 +178,6 @@ function main() {
 }
 
 function drawLandscape() {
-  /*ctx.fillStyle = "rgb(12, 35, 115)"
-  ctx.fillRect(0, 0, cnv.width, cnv.height); // sky
-
-  ctx.fillStyle = "rgb(120, 105, 180)";
-  ctx.fillRect(0, 450, cnv.width, 200); // ground
-
-  ctx.fillStyle = "rgb(255, 255, 180)"
-  fillCircle(395, 175, 40); // moon*/
-
   ctx.drawImage(images.background, 0, 0);
 
   ctx.drawImage(images.stump, (cnv.width / 2) - 100, 530);
@@ -188,7 +189,7 @@ document.addEventListener("keyup", handleKeyAction);
 
 function handleKeyAction(event) {
   if(event.type === "keydown" && keydown == false) {
-    //keydown = true;
+    keydown = true;
     if(event.code == "ArrowRight") {
       logChop("right");
     } else if(event.code == "ArrowLeft") {
@@ -231,7 +232,7 @@ function logChop(side) {
     //check if new bottom log is on player
     for(let i = 0; i < logs.length; i++) {
       if(logs[i].position == 0 && logs[i].branch == playerSide * -1) {
-        //gamestate++;
+        gamestate++;
         break;
       }
     }
@@ -239,7 +240,15 @@ function logChop(side) {
     // add another log on top
     let branch = Math.round(Math.random() * 3 - 1.5);
     if(branch == -0) {branch = 0};
-
+  
+    let log = document.getElementById("log");
+    if(Math.random() > 0.35) {
+      log = document.getElementById("log");
+    } else {
+      log = document.getElementById("log2");
+    }
+  
+  
     logs.push({
       x: (cnv.width / 2) - 75,
       y: 0,
@@ -247,11 +256,10 @@ function logChop(side) {
       position: 7,
       angle: 0,
       angleChange: Math.random() * 10 - 5,
-      branch: branch
+      branch: branch,
+      img: log
     })
-  } else if(gamestate == 2) { //also technically redundant
-    ctx.fillStyle = "black";
-    ctx.fillRect(50, 50, cnv.width - 50, cnv.height - 50);
+
   }
 
 }
